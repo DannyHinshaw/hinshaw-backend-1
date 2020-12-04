69086005af2b8b6e9fc01aea1a0421fe3a2b6ed3
dannyhinshaw <danny@52inc.com>
2020-12-04 06:29:34 -0500

    Basic db package scaffolded.

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module hinshaw-backend-1
 go 1.15
 
 require (
+	github.com/jackc/pgx/v4 v4.10.0
 	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo/v4 v4.1.17
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.5.1
 	golang.org/x/crypto v0.0.0-20201203163018-be400aefbc4c
 )
--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"hinshaw-backend-1/db"
 	"hinshaw-backend-1/handlers"
 	mw "hinshaw-backend-1/middleware"
 	"log"
+	"os"
 )
 
 func main() {
@@ -16,9 +18,20 @@ func main() {
 		log.Printf("godotenv.Load: %v\n", err)
 	}
 
+	// Init DB and prep for handlers DI.
+	err := db.ParseDB(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	// Setup Echo framework
 	e := echo.New()
-	h := handlers.NewHandler()
+	h := handlers.NewHandler(db.DatabaseService)
 
 	// Setup middlewares
 	e.Use(middleware.CORS())
